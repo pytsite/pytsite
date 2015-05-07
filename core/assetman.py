6f0ce68ca77f6e6d766562705882abbae3747b63
Alexander Shepetko <a@shepetko.com>
2015-05-07 15:07:07 +0300

    Works on assetman

--- core/assetman.py
+++ core/assetman.py
@@ -14,18 +14,39 @@ def register_package(package_name: str, assets_dir: str='assets'):
         raise Exception("Package '{0}' is not found.".format(package_name))
 
     from os import path
-    assets_dir = path.join(path.dirname(spec.origin), assets_dir)
-    if not path.isdir(assets_dir):
-        raise Exception("Directory '{0}' is not exists.".format(assets_dir))
+    assets_abs_dir = path.join(path.dirname(spec.origin), assets_dir)
+    if not path.isdir(assets_abs_dir):
+        raise Exception("Directory '{0}' is not exists.".format(assets_abs_dir))
 
-    from webassets import Bundle
     __packages[package_name] = {
-        '_path': assets_dir,  # absolute path to the package's assets dir
-        '_bundle': Bundle(),  # weassets' bundle
+        'path': assets_dir,
+        'assets': {'js': [], 'compressed_js': [], 'css': [], 'compressed_css': [], 'other': []},
     }
 
 
 def add(path: str):
+    """Add an asset.
+    """
+    package_name, asset_path, bundle_name = __split_asset_path_info(path)
+
+    assets_list = __packages[package_name]['assets'][bundle_name]
+    assets_list.append(asset_path)
+
+
+def url(path: str)->str:
+    """Get URL of an asset.
+    """
+    package_name, asset_path, bundle_name = __split_asset_path_info(path)
+    assets_list = __packages[package_name]['assets'][bundle_name]
+    if asset_path not in assets_list:
+        raise Exception("Asset '{0}' is not defined in package '{1}'.".format(asset_path, package_name))
+
+    from . import router
+    print(router.base_url())
+
+def __split_asset_path_info(path: str)->dict:
+    """Split asset path into package name and asset path.
+    """
     path_parts = path.split('@')
     package_name = 'app'
     asset_path = path
@@ -36,8 +57,15 @@ def add(path: str):
     if package_name not in __packages:
         raise Exception("Package '{0}' is not registered.".format(package_name))
 
-    bundle = __packages[package_name]['_bundle']
-    bundle.contents.append(asset_path)
+    if asset_path.endswith('.js'):
+        bundle_name = 'js'
+    elif asset_path.endswith('.min.js'):
+        bundle_name = 'compressed_js'
+    elif asset_path.endswith('.css'):
+        bundle_name = 'css'
+    elif asset_path.endswith('.min.css'):
+        bundle_name = 'compressed_css'
+    else:
+        bundle_name = 'other'
 
-    print(package_name)
-    print(asset_path)
+    return package_name, asset_path, bundle_name
--- core/router.py
+++ core/router.py
@@ -1,6 +1,9 @@
-from werkzeug.routing import Map as __Map
+from werkzeug.routing import Map as __Map, MapAdapter as __MapAdapter
 
 __routes = __Map()
+
+__url_adapter = None
+""":type : __MapAdapter"""
 
 
 def add_rule(pattern: str, endpoint: str, defaults: dict=None, methods=None, redirect_to: str=None):
@@ -24,11 +27,20 @@ def dispatch(env: dict, start_response: callable):
     """
     from werkzeug.exceptions import HTTPException, NotFound
     from werkzeug.wrappers import Request, Response
+    from werkzeug.utils import redirect
     from importlib import import_module
 
-    adapter = __routes.bind_to_environ(env)
+    global __url_adapter
+    __url_adapter = __routes.bind_to_environ(env)
+
+    path_info = __url_adapter.path_info
+
+    # Force trailing slash
+    if not path_info.endswith('/'):
+        return redirect(path_info + '/' + __url_adapter.query_args, 301)(env, start_response)
+
     try:
-        endpoint_str, values = adapter.match()
+        endpoint_str, values = __url_adapter.match()
 
         endpoint = endpoint_str.split('::')
         if len(endpoint) != 2:
@@ -62,3 +74,22 @@ def dispatch(env: dict, start_response: callable):
 
     except HTTPException as e:
         return e(env, start_response)
+
+
+def base_path():
+    return '/'
+
+
+def base_url():
+    from . import registry
+
+    server_name = registry.get_val('console.server_name', 'localhost')
+    scheme = 'http'
+
+    if __url_adapter:
+        server_name = __url_adapter.server_name
+        scheme = __url_adapter.url_scheme
+
+    redirect_url = scheme + '://' + server_name + base_path()
+
+    return
