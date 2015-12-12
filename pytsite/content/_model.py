a983c675b62479330798604253e22e167d1b142a
Alexander Shepetko <a@shepetko.com>
2015-12-12 10:18:00 +0200

    Release 0.39.2

--- pytsite/content/_model.py
+++ pytsite/content/_model.py
@@ -5,7 +5,7 @@ from datetime import datetime as _datetime, timedelta as _timedelta
 from pytsite import auth as _auth, taxonomy as _taxonomy, odm_ui as _odm_ui, route_alias as _route_alias, \
     geo as _geo, image as _image, ckeditor as _ckeditor, odm as _odm, widget as _widget, validation as _validation, \
     html as _html, router as _router, lang as _lang, assetman as _assetman, events as _events, mail as _mail, \
-    tpl as _tpl, auth_ui as _auth_ui
+    tpl as _tpl, auth_ui as _auth_ui, reg as _reg
 
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
@@ -243,7 +243,7 @@ class Content(_odm_ui.Model):
         self.f_set('body', body).f_set('images', images)
 
         # Changing status if necessary
-        if self.is_new:
+        if self.is_new and _reg.get('env.type') != 'console':
             if not current_user.has_permission('pytsite.content.bypass_moderation.' + self.model):
                 self.f_set('status', 'waiting')
             elif not self.status:
