7106caea5ad755e80868d38cd3407efe591c7436
Alexander Shepetko <a@shepetko.com>
2016-03-13 04:05:18 +0200

    AdminLTE theme updated
    AdminLTE theme updated from 2.1.0 to 2.3.2

--- pytsite/admin/_api.py
+++ pytsite/admin/_api.py
@@ -1,6 +1,7 @@
 """Admin API Functions
 """
-from pytsite import tpl as _tpl, widget as _widget
+from pytsite import tpl as _tpl, widget as _widget, core_version_str as _version_str, core_url as _core_url, \
+    core_name as _core_name, browser as _browser, assetman as _assetman
 
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
@@ -10,7 +11,17 @@ __license__ = 'MIT'
 def render(content: str) -> str:
     """Render admin page with content.
     """
+    _browser.include('bootstrap') 
+    _browser.include('font-awesome')
+    _assetman.add('pytsite.admin@AdminLTE/css/AdminLTE.min.css')
+    _assetman.add('pytsite.admin@AdminLTE/css/skins/skin-blue.min.css')
+    _assetman.add('pytsite.admin@css/custom.css')
+    _assetman.add('pytsite.admin@AdminLTE/js/app.js')
+    
     return _tpl.render('pytsite.admin@html', {
         'admin_language_nav': _widget.select.LanguageNav('admin-language-nav', dropdown=True),
         'content': content,
+        'core_name': _core_name,
+        'core_url': _core_url,
+        'core_version': _version_str(),
     })
--- pytsite/admin/ep.py
+++ pytsite/admin/ep.py
@@ -1,4 +1,5 @@
-from pytsite import metatag as _metatag, tpl as _tpl, lang as _lang, widget as _widget
+from pytsite import metatag as _metatag, lang as _lang
+from . import _api
 
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
@@ -10,14 +11,4 @@ def dashboard(args: dict, inp: dict):
     """
     _metatag.t_set('title', _lang.t('pytsite.admin@dashboard'))
 
-    app_name = _lang.t('app_name')
-    try:
-        app_name_short = _lang.t('app_name_short', exceptions=True)
-    except _lang.error.TranslationError:
-        app_name_short = app_name
-
-    return _tpl.render('pytsite.admin@html', {
-        'admin_language_nav': _widget.select.LanguageNav('admin-language-nav', dropdown=True),
-        'app_name': app_name,
-        'app_name_short': app_name_short,
-    })
+    return _api.render('')
