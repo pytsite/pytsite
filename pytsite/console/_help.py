26cf0e0367abdee5e813bf1fe387390dd9c675ae
Alexander Shepetko <a@shepetko.com>
2017-11-26 20:58:13 +0200

    Release 6.0

--- pytsite/console/_help.py
+++ pytsite/console/_help.py
@@ -1,6 +1,6 @@
-"""Help Command.
+"""PytSite Console Help Command
 """
-from . import _command, _api
+from . import _command, _api, _error
 
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
@@ -8,22 +8,25 @@ __license__ = 'MIT'
 
 
 class Help(_command.Command):
-    """Help Command.
+    """PytSite Console Help Command
     """
 
     @property
     def name(self) -> str:
-        """Get name of the command.
+        """Get name of the command
         """
         return 'help'
 
     @property
     def description(self) -> str:
-        """Get description of the command.
+        """Get description of the command
         """
         return 'pytsite.console@help_command_description'
 
     def exec(self):
-        """Execute the command.
+        """Execute the command
         """
+        if not self.args:
+            raise _error.MissingArgument()
+
         _api.print_info(_api.get_command(self.arg(0)).signature)
